fa281d043f5978b0093acfd8278d118db63494bc
Vitor <vitorlsboliveira@gmail.com>
2024-07-21 09:46:08 -0300

    Finalização do projeto com a adição da funcionalidade de consertar a lâmpada

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var lampada = document.getElementById('lampada')
 
+// Liga a lâmpada
 function Ligar() {
 	if (!estaQuebrada()) {
 		lampada.src = 'img/ligada.svg'
@@ -8,6 +9,7 @@ function Ligar() {
 	}
 }
 
+// Desliga a lâmpada
 function Desligar() {
 	if (!estaQuebrada()) {
 		lampada.src = 'img/desligada.svg'
@@ -16,10 +18,28 @@ function Desligar() {
 	}
 }
 
+// Quebra a lâmpada
 function Quebrar() {
 	lampada.src = 'img/quebrada.svg'
 }
 
 function estaQuebrada() {
 	return lampada.src.indexOf('quebrada') > -1
+}
+
+// Torna disponível o botão "Consertar" quando a lâmpada quebra
+function mostraBotaoConsertar() {
+	var botao_consertar = document.getElementById('botao_consertar')
+	if (!estaQuebrada) {
+		botao_consertar.style.display = 'none'
+	} else {
+		botao_consertar.style.display = 'inline-block'
+	}
+}
+lampada.addEventListener('click', mostraBotaoConsertar)
+
+// Conserta a lâmpada e torna indisponível o botão "Consertar"
+function Consertar() {
+	lampada.src = 'img/desligada.svg'
+	botao_consertar.style.display = 'none'
 }
